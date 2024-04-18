fd6c674d2d81a498c02419a401764d0b459f2c96
Bacteria007 <123715803+Bacteria007@users.noreply.github.com>
2024-04-18 13:29:18 +0500

    tasks list make global

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useState } from 'react'
+import ApiCaller from '../utilities/ApiCaller';
 
 const AppContext = createContext()
 export const useAppContext = () => useContext(AppContext);
@@ -6,11 +7,28 @@ export const useAppContext = () => useContext(AppContext);
 export const ContextProvider = ({ children }) => {
 
     const statusOptions = ['Todo', 'In Progress', 'Completed']
+    const token = localStorage.getItem("token")
+    const [tasks, setTasks] = useState([])
     const serverUrl = 'https://task-manager-api-ten.vercel.app'
-    // const serverUrl = window.location.origin
+    const getAllTasks = () => {
+        ApiCaller(`${serverUrl}/task/get-tasks`, {}, token).then(async (res) => {
+            const tasks = await res.data.data
+            if (res.data.status) {
+                setTasks(tasks)
+                console.log(res.data.data);
+            } else {
+                console.log(res.data.message)
+            }
+        })
+            .catch((error) => {
+                console.error("Error fetching task:", error);
+            });
+    }
+
     return (
         <AppContext.Provider value={{
-            serverUrl, statusOptions
+            serverUrl, statusOptions, tasks,
+            getAllTasks,
         }}>
             {children}
         </AppContext.Provider>
